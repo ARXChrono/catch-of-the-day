08a1ce341247c68b1982f8d6ee841fb875813191
Shaun Wong <ChronoXP@hotmail.com>
2018-12-08 20:34:47 +0800

    Create App Layout
    Load CSS, And Create App Layout with Components

---
+++ src/components/Inventory.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Inventory extends React.Component {
+  render() {
+    return(
+      <div className="Inventory">
+        Inventory!
+      </div>
+    )
+  }
+}
+
+export default Inventory;
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render(){
-    return <p>i am the store picker!</p>
+    return (
+      <form className="store-selector">
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholer="Store Name" />
+        <button type="submit"> Visit Store </button>
+      </form>
+    )
   }
 }
 
