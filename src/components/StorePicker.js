ed9ece42d2b16a2cfb458ab92a297f8cb3dd090a
Shaun Wong <ChronoXP@hotmail.com>
2018-12-08 23:44:25 +0800

    Helper Functions
    Using Helper functions

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { getFunName } from '../helpers';
+
 
 class StorePicker extends React.Component {
   render(){
@@ -6,6 +8,7 @@ class StorePicker extends React.Component {
       <form className="store-selector">
         <h2>Please Enter A Store</h2>
         <input type="text" required placeholer="Store Name" />
+        <input type="text" required placeholer="Store Name" defaultValue={getFunName()}/>
         <button type="submit"> Visit Store </button>
       </form>
     )
