927844c7e65b9cca33e9d5498724990d67e29a19
Shaun Wong <ChronoXP@hotmail.com>
2018-12-09 02:35:00 +0800

    Update State

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -17,7 +17,9 @@ class AddFishForm extends React.Component {
       desc: this.descRef.value.value,
       image: this.imageRef.value.value,
     };
-     console.log(fish);
+    this.props.addFish(fish);
+    // reset form
+    event.currentTarget.reset();
   };
 
   render() {
--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,20 @@ class App extends React.Component {
   };
 
   addFish = fish => {
-    console.log("Adding a fish 🐟");
+
+    // 1. take a copy of existing state
+    const fishes = { ...this.state.fishes };
+
+    // 2. Add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+
+    // 3 . Set the new fishes object to state
+    this.setState({
+      fishes: fishes
+    });
+
+    // console.log("Adding a fish 🐟");
+    // this.state.fishes.push(push);
   }
 
   render() {
