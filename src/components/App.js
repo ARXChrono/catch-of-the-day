0be2da472193377672b446a175076b6a2118b930
Shaun Wong <ChronoXP@hotmail.com>
2018-12-09 21:05:33 +0800

    Loading data into state onClick

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
+import sampleFishes from '../sample-fishes';
 
 
 class App extends React.Component {
@@ -28,6 +29,13 @@ class App extends React.Component {
     // this.state.fishes.push(push);
   }
 
+  loadSampleFishes = () => {
+    // alert('loading samples!');
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -35,7 +43,7 @@ class App extends React.Component {
           <Header tagline="Shaun is cool" age={500} cool={true} />
         </div>
           <Order />
-          <Inventory addFish={this.addFish}/>
+          <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
       </div>
     );
   }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -7,6 +7,7 @@ class Inventory extends React.Component {
       <div className="Inventory">
         <h2>Inventory</h2>
         <AddFishForm addFish={this.props.addFish}/>
+        <button onClick={this.props.loadSampleFishes}> Load Sample Fishes </button>
       </div>
     );
   }
