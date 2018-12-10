88a91f46d7abc32441bf91cd5eca61bcf3f79e11
Shaun Wong <ChronoXP@hotmail.com>
2018-12-10 23:12:24 +0800

    Updating Order State

--- src/components/App.js
+++ src/components/App.js
@@ -38,13 +38,29 @@ class App extends React.Component {
     });
   }
 
+  addToOrder = (key) => {
+    // 1. take a copy of state
+    const order = { ...this.state.order };
+    // 2. Either add to the order, or update the number in our order
+    order[key] = order[key] + 1 || 1 ;
+    // 3. Call SetState to update our state object
+    this.setState({order});
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Shaun is cool" age={500} cool={true} />
           <ul className="fishes">
-            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                index={key} // must pass key as prop a 2nd time to be used
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
           </ul>
         </div>
           <Order />
--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,10 +2,14 @@ import React from 'react';
 import {formatPrice} from "../helpers";
 
 class Fish extends React.Component {
+  handleClick = () => {
+    this.props.addToOrder(this.props.index);
+  }
   render() {
     // const image = this.props.details.fish.image;
     // use es6 destructuring to break it down into var
-    const { image, name , price, desc, /*status*/ } = this.props.details;
+    const { image, name , price, desc, status } = this.props.details;
+    const isAvailable = status === 'available';
     return(
       <li className="menu-fish">
         <img src={image} alt={name} />
@@ -13,7 +17,9 @@ class Fish extends React.Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add To cart</button>
+        <button disabled={!isAvailable} onClick={this.handleClick}>
+          {isAvailable ? "Add To Order" : 'Sold Out!'}
+        </button>
       </li>
     )
   }
