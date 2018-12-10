d98f87e172d3a405542c8a56513836404e41e4c5
Shaun Wong <ChronoXP@hotmail.com>
2018-12-10 23:45:01 +0800

    Displaying Order State - Clean Up RenderOrder

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,6 +2,21 @@ import React from 'react';
 import {formatPrice} from "../helpers";
 
 class Order extends React.Component {
+  renderOrder = key => {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+    const isAvailable = fish && fish.status === "available";
+    if(!isAvailable) {
+      return <li key={key}> Sorry { fish ? fish.name : fish } is not available</li>
+    }
+    return (
+    <li key={key}>
+      {/*key*/}
+      { count } lbs {fish.name}
+      {formatPrice(count * fish.price)}
+    </li>
+    );
+  }
   render() {
     const orderIds = Object.keys(this.props.order);
     const total = orderIds.reduce((prevTotal, key) => {
@@ -16,8 +31,8 @@ class Order extends React.Component {
     return(
       <div className="order-wrap">
         <h2>Order</h2>
-        <ul>
-          {orderIds.map(key => <li>{key}</li>)}
+        <ul className="order">
+          {orderIds.map(this.renderOrder)}
           <div className="total">
             Total: <strong>{formatPrice(total)}</strong>
           </div>
